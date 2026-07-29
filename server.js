241122d6f5a7067a09abcf41c732efa65489eb30
Your Name <you@example.com>
2026-07-29 13:13:07 +0530

    new updated code

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://api.ednixa.com"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
